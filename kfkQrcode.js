e8072250d89fb4b1cf76dc728068bc3339dfbb17
hjd919 <hjd919@126.com>
2020-10-24 17:22:09 +0800

    sync

--- kfkQrcode.js
+++ kfkQrcode.js
@@ -155,8 +155,6 @@ async function fullScreenshot(link, mobile, num) {
             const payres = await page.waitForResponse(response => response.url().indexOf("get_order_state") > -1 && response.status() === 200);
             const json = await payres.json()
             if (json.data.code == 0) {
-                let payresPostdata = payres.postData()
-                let ordernum = payresPostdata.split("=")[1]
                 paid = true
                 redis.client.set(`paid:${mobile}`, `1`);
                 break
