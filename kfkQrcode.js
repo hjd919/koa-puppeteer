3041e101b4f0cbf26752a066adf496afdb68a037
hjd919 <hjd919@126.com>
2020-10-24 17:45:56 +0800

    sync

--- kfkQrcode.js
+++ kfkQrcode.js
@@ -16,7 +16,9 @@ const mobile = argv[3]
 const num = argv[4]
 process.on('error', (error) => {
     console.log('子进程错误', error);
-    process.send({error})
+    process.send({
+        error
+    })
 });
 
 // 使父进程输出: 父进程收到消息 { foo: 'bar', baz: null }
@@ -36,6 +38,7 @@ const blockedResourceTypes = [
     'csp_report',
     'imageset',
 ];
+
 function shot(page, num) {
     return page.screenshot({
         path: num + '.png'
@@ -59,7 +62,7 @@ async function fullScreenshot(link, mobile, num) {
             '-—disable-dev-tools'
         ]
     });
- 
+
     const page = await browser.newPage();
 
     // 打印浏览器信息
@@ -80,114 +83,107 @@ async function fullScreenshot(link, mobile, num) {
     page.setUserAgent("Mozilla/6.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.0 Safari/537.36")
 
     // try {
-        let url = "https://www.kuaifaka.com/purchasing?link=" + link
-    
-        await page.goto(url, {
-            waitUntil: ['domcontentloaded', 'load', 'networkidle0']
-        });
-        
-        const elem = await page.$('div');
-        const boundingBox = await elem.boundingBox();
-        await page.mouse.move(
-            boundingBox.x + boundingBox.width / 2,
-            boundingBox.y + boundingBox.height / 2
-        );
-        await page.mouse.wheel({
-            deltaY: 1000
-        })
-        
-        await page.click('#purchasing_sp > div.ure_info_box > div.ure_info > div:nth-child(1) > div.input > input');
-        await page.keyboard.type(mobile);
-        
-        // 先删除原来的值
-        await page.click("#purchasing_sp > div.ford > div > div.shuliang_box > div.input")
-        await page.keyboard.down('Shift');
-        await page.keyboard.press('ArrowRight');
-        await page.keyboard.press('Backspace');
+    let url = "https://www.kuaifaka.com/purchasing?link=" + link
 
-        // 输入数量
-        await page.keyboard.type(num);
-    
-        await page.evaluate(() => {
-            document.querySelector('.qued_btn').click()
-            return ""
-        });
-   
-        let selector
-        selector = '#last_order_box > div.queding_box > div > span:nth-child(2)'
+    await page.goto(url, {
+        waitUntil: ['domcontentloaded', 'load', 'networkidle0']
+    });
+
+    const elem = await page.$('div');
+    const boundingBox = await elem.boundingBox();
+    await page.mouse.move(
+        boundingBox.x + boundingBox.width / 2,
+        boundingBox.y + boundingBox.height / 2
+    );
+    await page.mouse.wheel({
+        deltaY: 1000
+    })
+
+    await page.click('#purchasing_sp > div.ure_info_box > div.ure_info > div:nth-child(1) > div.input > input');
+    await page.keyboard.type(mobile);
+
+    // 先删除原来的值
+    await page.click("#purchasing_sp > div.ford > div > div.shuliang_box > div.input")
+    await page.keyboard.down('Shift');
+    await page.keyboard.press('ArrowRight');
+    await page.keyboard.press('Backspace');
+
+    // 输入数量
+    await page.keyboard.type(num);
+
+    await page.evaluate(() => {
+        document.querySelector('.qued_btn').click()
+        return ""
+    });
+
+    let selector
+    selector = '#last_order_box > div.queding_box > div > span:nth-child(2)'
+    await page.waitForSelector(selector);
+    await page.click(selector)
+
+    await page.waitFor(2000);
+    selector = '#confirm_order_number > div.btn_box > button'
+    try {
         await page.waitForSelector(selector);
-        await page.click(selector)
-     
-        try {
-            await page.waitFor(2000);
-        } catch (error) {
-            process.send({
-                "error 2000": error
-            });
-        }
-        
-        selector = '#confirm_order_number > div.btn_box > button'
-        try {
-            await page.waitForSelector(selector);
-        } catch (error) {
-            process.send({
-                "error page.click": error
-            });
-        }
-      
-        await page.click(selector)
- 
-        const finalRequest = await page.waitForRequest(request => request.url().indexOf("qrCode") > -1 && request.method() === 'GET');
-        const qrcode = finalRequest.url()
+    } catch (error) {
         process.send({
-            qrcode
+            "error page.click": error
         });
-        redis.client.setex(`qrcode:${mobile}`, 5 * 60, qrcode);
+    }
+    await shot(page, "133")
+    await page.click(selector)
 
-        const eachTime = 3 * 1000
-        const totalTime = 5 * 60 * 1000
-        // const eachTime = 2 * 1000
-        // const totalTime = 4 * 1000
-        let totalNum = parseInt(totalTime / eachTime)
-        let paid = false
-        let isset = false
-        for (let index = 0; index < totalNum; index++) {
-            const payres = await page.waitForResponse(response => response.url().indexOf("get_order_state") > -1 && response.status() === 200);
-            const json = await payres.json()
-            if (json.data.code == 0) {
-                paid = true
-                redis.client.set(`paid:${mobile}`, `1`);
-                break
-            }
-            if (!isset) {
-                let payresPostdata = payres.request().postData()
-                let ordernum = payresPostdata.split("=")[1]
-                redis.client.set(`paid:${mobile}`, `0`);
-                redis.client.set(`ordernum:${mobile}`, ordernum);
-                isset = true
-            }
-            process.send({
-                index,
-                json
-            });
+    const finalRequest = await page.waitForRequest(request => request.url().indexOf("qrCode") > -1 && request.method() === 'GET');
+    const qrcode = finalRequest.url()
+    process.send({
+        qrcode
+    });
+    redis.client.setex(`qrcode:${mobile}`, 5 * 60, qrcode);
+
+    const eachTime = 3 * 1000
+    const totalTime = 5 * 60 * 1000
+    // const eachTime = 2 * 1000
+    // const totalTime = 4 * 1000
+    let totalNum = parseInt(totalTime / eachTime)
+    let paid = false
+    let isset = false
+    for (let index = 0; index < totalNum; index++) {
+        const payres = await page.waitForResponse(response => response.url().indexOf("get_order_state") > -1 && response.status() === 200);
+        const json = await payres.json()
+        if (json.data.code == 0) {
+            paid = true
+            redis.client.set(`paid:${mobile}`, `1`);
+            break
         }
-
+        if (!isset) {
+            let payresPostdata = payres.request().postData()
+            let ordernum = payresPostdata.split("=")[1]
+            redis.client.set(`paid:${mobile}`, `0`);
+            redis.client.set(`ordernum:${mobile}`, ordernum);
+            isset = true
+        }
         process.send({
-            paid
+            index,
+            json
         });
+    }
 
-        if (!paid) {
-            page.close();
-            browser.close();
-            return
-        }
+    process.send({
+        paid
+    });
 
-        const cardsqueryres = await page.waitForResponse(response => response.url().indexOf("cards_query") > -1 && response.status() === 200);
-        let cardsquery = await cardsqueryres.text()
-        redis.client.set(`cards_query:${mobile}`, cardsquery);
-
+    if (!paid) {
         page.close();
         browser.close();
+        return
+    }
+
+    const cardsqueryres = await page.waitForResponse(response => response.url().indexOf("cards_query") > -1 && response.status() === 200);
+    let cardsquery = await cardsqueryres.text()
+    redis.client.set(`cards_query:${mobile}`, cardsquery);
+
+    page.close();
+    browser.close();
 }
 
 
